e33e6779ec4e7f9b74411df358c797443b28ea2c
Emiliano Nahuel Garcia <enahuelgarcia@gmail.com>
2024-11-13 12:41:30 -0300

    Colocando Sprites de prueba

--- configuracion.py
+++ configuracion.py
@@ -1,13 +1,22 @@
 import pygame
 
 
-ANCHO = 1200
+ANCHO = 1000
 
 ALTO = 800
 
 NOMBRE_JUEGO = "Developers Game DIV 115"
 
 ICONO = pygame.image.load("assets/images/logo_juego.png")
+
+FONDO = pygame.image.load("assets/images/fondo_juego.jpg")
+FONDO = pygame.transform.scale(FONDO, (1000, 800))
+
+darth_vader = pygame.image.load("assets/images/darth_vader.png")
+darth_vader = pygame.transform.scale(darth_vader, (200, 200))
+
+mickey = pygame.image.load("assets/images/mickey.png")
+mickey = pygame.transform.scale(mickey, (150, 150))
 
 pygame.display.set_icon(ICONO)
 
@@ -16,3 +25,4 @@ ROJO = (255, 0, 0)
 AZUL = AZUL = (0, 0, 255)
 
 FUENTE = pygame.font.Font(None, 36)
+
--- main.py
+++ main.py
@@ -2,42 +2,55 @@ import pygame
 pygame.init()
 import random
 import sys
-from configuracion import (
-    ANCHO,
-    ALTO,
-    NOMBRE_JUEGO,
-    ROJO,
-    AZUL,
-    FUENTE
-    )
+from configuracion import *
 
 
 
 
-ventana = pygame.display.set_mode((ANCHO, ALTO), pygame.RESIZABLE)
+ventana = pygame.display.set_mode((ANCHO, ALTO)) #creacion de la ventana
 
-pygame.display.set_caption(NOMBRE_JUEGO)
+pygame.display.set_caption(NOMBRE_JUEGO) #nombre del juego en la ventana
+
+cuadrado = pygame.Surface((200, 200)) # crea la superficie
+cuadrado.fill(ROJO)
+
+
+
+texto = FUENTE.render("DEVELOPERS GAME LA GUERRA DE LAS DIVISIONES", True, AZUL) #dibuja un texto en la ventana
+
+#sonido_disparo = pygame.mixer.Sound(disparo.waw) PARA LOS TIROS WACHO
+
+
+
+
 
 jugando = True
-
-
-while jugando:
+while jugando: #bucle principal del juego
     
-
-    for evento in pygame.event.get():
+    
+    for evento in pygame.event.get(): #captura los eventos del juego
         
         if evento.type == pygame.QUIT:
             jugando = False
+        
+        if evento.type == pygame.KEYDOWN:
+            print(f"tecla {evento.key}")
+            #if evento.key == pygame.K_SPACE:
+                #sonido_disparo.play()
+            
+                
 
-        pygame.display.update()
-    
-    ventana.fill(ROJO)
-    
-    x, y = pygame.mouse.get_pos()
+     
+    ventana.blit(FONDO, (0,0)) #pega el fondo
+    ventana.blit(darth_vader, (250, 600)) #pega el sprite
+    ventana.blit(mickey, (350, 600))
+    x, y = pygame.mouse.get_pos() #captura el mouse
 
-    texto = FUENTE.render("DEVELOPERS GAME LA GUERRA DE LAS DIVISIONES", True, AZUL)
+    
     
 
-    ventana.blit(texto, (20, 20))
+    ventana.blit(texto, (20, 20)) #pega el texto 
+    
+    pygame.display.update() #updatea la pantalla dentro del bucle para notar cambios
 
 pygame.quit()
