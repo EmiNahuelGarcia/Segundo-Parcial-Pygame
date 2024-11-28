53715e7f83516894928dae03cd957b80372cd81c
DaianaColq <daiana.colq@hotmail.com>
2024-11-27 23:04:42 -0300

    modularizacion de movimientos

--- funciones_movimientos.py
+++ funciones_movimientos.py
@@ -1,26 +1,27 @@
 from configuracion import *
 from personajes import *
 
+def invertir_sprites_player(sprites, personaje):
+    keys = ["inactivo", "corriendo_1", "corriendo_2"]
+    for key in keys:
+        sprites[key] = pygame.transform.flip(sprites[key], True, False)
+    personaje["vista"] = "izquierda" if personaje["vista"] == "derecha" else "derecha"
 
-
-
+def handle_girar(teclas, personaje, sprites):
+    if teclas[pygame.K_LEFT] and personaje["vista"] == "derecha":
+            invertir_sprites_player(sprites, personaje)
+    elif teclas[pygame.K_RIGHT] and personaje["vista"] == "izquierda":
+            invertir_sprites_player(sprites, personaje)
+    return sprites
 
 def mover_personaje(personaje, rect_personaje, teclas, sprites):
     """Mueve al personaje y actualiza su posición en el rectángulo."""
     # Movimiento horizontal
-    if teclas[pygame.K_LEFT] or teclas[pygame.K_RIGHT]:
 
-        if teclas[pygame.K_LEFT] and personaje["vista"] == "derecha":
-            print("GIRO A IZQUIERDA")
-            sprites["inactivo"] = pygame.transform.flip(sprites["inactivo"], True, False)
-            personaje["vista"] = "izquierda"
-
-        if teclas[pygame.K_RIGHT] and personaje["vista"] == "izquierda":
-            print("GIRO A DERECHA")
-            sprites["inactivo"] = pygame.transform.flip(sprites["inactivo"], True, False)
-            personaje["vista"] = "derecha"
-
-        # Cambiar entre los dos sprites de correr
+    if not (teclas[pygame.K_LEFT] or teclas[pygame.K_RIGHT]):
+        personaje["sprite actual"] = "inactivo"
+        
+    else:
         if personaje.get("ultimo_cambio", 0) + 150 < pygame.time.get_ticks():  # Cambiar cada 150ms
             personaje["ultimo_cambio"] = pygame.time.get_ticks()
             # Alternar entre los dos sprites de correr
@@ -30,23 +31,18 @@ def mover_personaje(personaje, rect_personaje, teclas, sprites):
                 personaje["sprite actual"] = "corriendo_1"
 
         # Movimiento a la izquierda
+        handle_girar(teclas, personaje, sprites)
+
         if teclas[pygame.K_LEFT]:
             rect_personaje.x -= personaje["velocidad x"]
-            # Voltear el sprite horizontalmente cuando va a la izquierda
-            '''sprite_volteado = pygame.transform.flip(sprites[personaje["sprite actual"]], True, False)
-            sprites[personaje["sprite actual"]] = sprite_volteado'''
             if rect_personaje.left < 10:
                 rect_personaje.left = 10
-            
 
         # Movimiento a la derecha
         if teclas[pygame.K_RIGHT]:
             rect_personaje.x += personaje["velocidad x"]
             if rect_personaje.right > ANCHO -10:
                 rect_personaje.right = ANCHO -10
-            
-    else:
-        personaje["sprite actual"] = "inactivo"  # Si no se mueve, vuelve al sprite inactivo
         
 
 
@@ -90,4 +86,3 @@ def aplicar_gravedad(personaje, rect_personaje, plataformas):
     if personaje["velocidad y"] > 15:
         personaje["velocidad y"] = 15
     
-
